a68736cee4590ee4fe499bf59b7b1658b5d02f41
gulnarkaz <gulnarkaz@gmail.com>
2025-01-24 15:57:32 +0500

    Update main.py
    земенила return Response на raise HTTPException при не нахождении нужного id пользователя

--- hw-backend-2/users/app/main.py
+++ hw-backend-2/users/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, HTTPException
 from fastapi.templating import Jinja2Templates
 
 from .users import create_users
@@ -20,20 +20,11 @@ def get_all_users(request: Request):
         "users": users
         })
     
-#@app.get("/p_users")
-#def get_paginated_users(request: Request, response: Response, page: int = 1, limit: int = 10):
-#    start = (page - 1) * limit
-#    end = start + limit
-#    return templates.TemplateResponse("users/index.html", {
-#        "request": request, 
-#       "users": users[start:end]
-#        })
-
 @app.get("/users/{user_id}")
 def get_user(request: Request, response: Response, user_id: int):
     user = next((user for user in users if user["id"] == user_id), None)
     if user is None:
-        return Response("Not found", status_code=404)
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     return templates.TemplateResponse("users/user.html", {
         "request": request, 
         "user": user
